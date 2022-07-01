2f33db798f81905d109f9367122b5b54ce63bd18
Fabio Freitas <fabiof_costa1@hotmail.com>
2022-07-01 15:27:12 -0300

    Alterado GitIgnore e DML_DDL do BD

--- DML_DDL.sql
+++ DML_DDL.sql
@@ -13,6 +13,7 @@ CREATE TABLE public.estoque (
 	CONSTRAINT estoque_pkey PRIMARY KEY (id_estoque)
 );
 
+
 CREATE TABLE public.fornecedor (
 	id_fornecedor serial4 NOT NULL,
 	tipo varchar(255) NULL,
@@ -44,6 +45,7 @@ CREATE TABLE public.usuario (
 	nome_usuario varchar(100) NULL,
 	email varchar(100) NULL,
 	senha varchar(255) NULL,
+	foto_perfil varchar(255) NULL,
 	CONSTRAINT usuario_pkey PRIMARY KEY (id_usuario)
 );
 
@@ -108,4 +110,4 @@ INSERT INTO public.usuario (NOME_USUARIO,EMAIL,SENHA) VALUES
 	 ('usuario','usuario@mail.com','$2a$12$3COb/LHNYO/JeLWy1ExGFe6U2iyFczP70/kBUEHbf9miQ4Vp64A42');
 	
 INSERT INTO public.usuario_rel_perfil  (ID_USUARIO,ID_PERFIL) VALUES
-	 (1,1);
+	 (1,1);	
